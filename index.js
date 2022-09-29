1e90a808bfd7d81db35521c45029408d7cb55909
aframson <obiriaframson@gmail.com>
2022-09-29 16:08:18 +0000

    this

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const leaf = express()
 const http = require('http')
 const { Server } = require('socket.io')
 const Leaf = {
-    init: (port, obj, callback) => {
+    init:(port,obj,callback) => {
         const PORT = process.env.PORT || port // you can change the port here 
         const NODE_ENV = process.env.NODE_ENV || 'development'
         leaf.set('port', PORT); // set port 
@@ -11,7 +11,7 @@ const Leaf = {
         const server = http.createServer(leaf)
         const io = new Server(server, {
             cors: {
-                origin: obj.origin, // accept any origin you can change it to your specific url for security
+                origin:obj.origin, // accept any origin you can change it to your specific url for security
                 method: obj.methods // you can add more methods eg. DELETE , UPDATE etc...
             }
         })
@@ -21,7 +21,7 @@ const Leaf = {
             res.render('public')
         })
         // error handling 
-        require(obj.errorHandler)(leaf)
+        obj.errorHandler(leaf)
         // export the server 
         module.exports = server;
         // listern to the server
