69df1b187b4b3e5517c29907febb8cfbab23a8da
aframson <obiriaframson@gmail.com>
2022-10-09 01:49:17 +0000

    added loading balancing and service workers to increase performance

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const leaf = express()
 const http = require('http')
 const { Server } = require('socket.io')
-
+const process = require('process');
+const numCPUs = require('os').cpus().length;
+const cluster = require('cluster')
 const log = require('morgan')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
@@ -44,44 +46,60 @@ const Leaf = {
             // allow static files 
             app.use(express.static('./settings/public'));
         }
-      
+
 
     },
     init: (port, obj, callback) => {
         const PORT = process.env.PORT || port // you can change the port here 
         const NODE_ENV = process.env.NODE_ENV || 'development'
+        const isDev = process.env.NODE_ENV !== 'production'
         leaf.set('port', PORT); // set port 
         leaf.set('env', NODE_ENV); // set environment
-        const server = http.createServer(leaf)
-        const io = new Server(server, {
-            cors: {
-                origin: obj.origin, // accept any origin you can change it to your specific url for security
-                method: obj.methods // you can add more methods eg. DELETE , UPDATE etc...
+
+        // Multi-process to utilize all CPU cores.
+        if (cluster.isMaster) {
+            console.log(`
+    Master server ${process.pid} is running,
+    Leaf server is unitilizing ${numCPUs} CPU's for better 
+    performance and handling load balancing`);
+            console.log(`
+    Number of CPU's :`,numCPUs
+            )
+            // create workers for each cpu.
+            for (let i = 0; i < numCPUs; i++) {
+                cluster.fork();
             }
-        })
-        callback(io, leaf);
-        // introduction page begins with '/'
-        leaf.get('/', (req, res) => {
-            res.render('public')
-        })
-        // error handling 
-        obj.errorHandler(leaf)
-        // export the server 
-        module.exports = server;
-        // listern to the server
-        server.listen(PORT, () => {
-            console.log(
-                `
-    ============================================================
-
-    ✅ Leaf Server started @  http://localhost:${leaf.get('port')}.
-
-    ✅ Environment : ${leaf.get('env')}
-
-    ============================================================
+            cluster.on('exit', (worker, code, signal) => {
+                console.log(`server-worker ${worker.process.pid} died`);
+             
+            });
+        } else {
+            const server = http.createServer(leaf)
+            const io = new Server(server, {
+                cors: {
+                    origin: obj.origin, // accept any origin you can change it to your specific url for security
+                    method: obj.methods // you can add more methods eg. DELETE , UPDATE etc...
+                }
+            })  
+            callback(io, leaf);  
+            // introduction page begins with '/'
+            leaf.get('/', (req, res) => {
+                res.render('public')
+            })
+            // error handling 
+            obj.errorHandler(leaf)
+            // export the server 
+            module.exports = server;
+            // listern to the server
+            server.listen(PORT, () => {
+                console.log(
+                    `
+    ✅ Leaf Server-worker-${process.pid} started @  http://localhost:${leaf.get('port')} | Environment : ${leaf.get('env')}
+    ========================================================================================
             `
-            );
-        });
+                );
+            });
+        }
     }
 }
 
